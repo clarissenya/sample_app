ac8c8827b07cbb97136b62e6979506881b748106
izumi <291220145@qq.com>
2019-03-09 14:28:11 +0800

    Add acount activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,19 +1,10 @@
 require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
-  
-	def setup
-    @user = users(:michael)
-  end
 
-  test "index including pagination" do
-    log_in_as(@user)
-    get users_path
-    assert_template 'users/index'
-    assert_select 'div.pagination'
-    User.paginate(page: 1).each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-    end
+  def setup
+    @admin     = users(:michael)
+    @non_admin = users(:archer)
   end
 
   test "index as admin including pagination and delete links" do
@@ -38,5 +29,4 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_select 'a', text: 'delete', count: 0
   end
-  
 end
