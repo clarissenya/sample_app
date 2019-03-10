982555adc39c367f1e040bf5e71a2aad1b8d8e3a
izumi <291220145@qq.com>
2019-03-10 16:27:32 +0800

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-  	    @user = User.new(name: "Example User", email: "user@example.com",
+  	@user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
 
@@ -81,6 +81,35 @@ class UserTest < ActiveSupport::TestCase
     @user.microposts.create!(content: "Lorem ipsum")
     assert_difference 'Micropost.count', -1 do
       @user.destroy
+    end
+  end
+
+  test "should follow and unfollow a user" do 
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    assert michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
+
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # 关注的用户发布的微博
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # 自己的微博
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # 未关注用户的微博
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
     end
   end
 end
